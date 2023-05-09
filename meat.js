0c1a4850623240b83d085c2e62f3c562fd0bbf55
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2023-05-09 18:09:34 -0400

    allow emojis to be used in chat messages

--- meat.js
+++ meat.js
@@ -3117,7 +3117,7 @@ class User {
             return;
 
         let text = this.private.sanitize ? sanitize(sanitizeHTML(data.text)) : sanitizeHTML(data.text);
-		if (!/^[~`!@#$%^&*()_+=\w[\]\\{}|;':",.\//<>?\s\w&.\-б]*$/i.test(text) || !/^[~`!@#$%^&*()_+=\w[\]\\{}|;':",.\//<>?\s\w&.\-б]*$/i.test(text)) {
+		if (!/^[~`!@#$%^&*()_+=\w[\]\\{}|;':",.\//<>?\s\w&.\-б]*$/i.test(text) || !/^[~`!@#$%^&*()_+=\w[\]\\{}|;':",.\//<>?\s\w&.\-б]*$/i.test(text) || !/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/i.test(text)) {
 		  //if (this.getAgent() != "node-XMLHttpRequest") {
 			text = "You can only have english numeric, special and alphabetic characters.  <br><small>Only you can see this.</small>";
 			this.socket.emit("talk", {
